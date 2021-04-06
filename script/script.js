5c0ad16d7603abb944f18455d91e771fa30faad7
Gustav Kotze <edgegustavkotze@gmail.com>
2021-04-06 20:58:22 +0200

    UD

--- script/script.js
+++ script/script.js
@@ -1,12 +1,12 @@
 function storyForm(){
 const name = document.form1.name.value;
 const color = document.querySelectorAll("input[name=radio]:checked")[0].value;
-var animal = document.getElementById("Animal");
-var number = document.getElementById("number").value;
-var parts = document.getElementById("part");
-var area = document.getElementById("area");
-var senseArray = [];
-var checkboxes = document.querySelectorAll('input[type=checkbox]:checked');
+const animal = document.getElementById("Animal");
+const number = document.getElementById("number").value;
+const parts = document.getElementById("part");
+const area = document.getElementById("area");
+const senseArray = [];
+const checkboxes = document.querySelectorAll('input[type=checkbox]:checked');
 const word = document.form1.word.value;
 for (var i = 0; i < checkboxes.length; i++) {
   senseArray.push(checkboxes[i].value)
@@ -14,12 +14,31 @@ for (var i = 0; i < checkboxes.length; i++) {
 const animalName = animal.options[animal.selectedIndex].text;
 const partName = parts.options[parts.selectedIndex].text;
 const areaName = area.options[area.selectedIndex].text;
-const msg = "Hi " + name + " you " + color + " " + animalName + " with the " + number + " " + partName + " that live in a " + senseArray + " " + areaName + " with your best friend " + word + "!";
+const msg = "Your Friend " + name + ", the " + color + " " + animalName + " with the " + number + " " + partName + " lives in a " + senseArray + " " + areaName + " with his best friend " + word + "! Sorry He can't be your friend at the moment, he already has one.";
 console.log(msg);
 console.log(senseArray);
 document.getElementById("survey-form").style.display = "none";
-var window = document.createElement("h5");
+document.getElementById("Contact").style.display = "none";
+document.getElementById("Back").style.display = "block";
+document.getElementById("ContactButton").style.display = "none";
+const window = document.createElement("p");
 window.innerHTML = msg;
 
 document.body.appendChild(window); 
+}
+
+function Back(){
+
+ 
+document.getElementById("survey-form").style.display = "flex";
+document.getElementById("Contact").style.display = "flex";
+document.getElementById("Back").style.display = "none";
+document.getElementById("ContactButton").style.display = "block";
+const p = document.querySelector("p");
+if(p.style.display = "block"){
+  p.style.display = "none";
+} else{
+  p.style.display = "none";
+}
+  
 }
